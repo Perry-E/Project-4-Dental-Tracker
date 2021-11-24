2d2f73b6116a5fda2738037a499478009d3104a1
Perry-E <perryeber@gmail.com>
2021-11-24 17:19:06 +0800

    Added line chart component

---
+++ dentaltracker/src/Components/Analytics/LineChart.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export function LineChart(props) {
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Payments Received (Commission)",
+      },
+    },
+  };
+
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: [10, 20, 10, 20, 10, 10, 10, 20, 30, 10, 20, 50],
+        borderColor: "rgb(6, 214, 160)",
+        backgroundColor: "rgb(6, 214, 160, 0.5)",
+        tension: 0.1,
+      },
+      // {
+      //   label: "Dataset 2",
+      //   data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      //   borderColor: "rgb(53, 162, 235)",
+      //   backgroundColor: "rgba(53, 162, 235, 0.5)",
+      // },
+    ],
+  };
+
+  return (
+    <div
+      className="chart-container"
+      style={{ position: "relative", height: "50vh", width: "70vw" }}
+    >
+      <Line options={options} data={data} />
+    </div>
+  );
+}
