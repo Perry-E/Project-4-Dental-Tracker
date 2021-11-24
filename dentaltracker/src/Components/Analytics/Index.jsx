dfad1836f25099882a79831aa9e48444ed1254b8
Perry-E <perryeber@gmail.com>
2021-11-24 17:21:08 +0800

    Connected chart components to tab component and then added to index.jsx

---
+++ dentaltracker/src/Components/Analytics/Index.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
+import { useAuth } from "../../firebase";
+// import { format } from "date-fns";
+// import BarChart from "./BarChart";
+// import { LineChart } from "./LineChart";
+// import PieChart from "./PieChart";
+// import FullWidthTabs from "./Tab";
+import BasicTabs from "./Tab";
+
+export default function Analytics() {
+  const currentUser = useAuth();
+  const [procedureData, setProcedureData] = useState([]);
+  // const [month, setMonth] = useState();
+  // const [year, setYear] = useState();
+
+  // console.log("YEAR", year);
+  // console.log("MONTH", month);
+
+  useEffect(() => {
+    let procedureArray = [];
+    async function allProcedures() {
+      const procedureSubcollection = await getDocs(
+        collection(db, "users", `${currentUser?.uid}`, "procedures")
+      );
+      procedureSubcollection?.forEach((doc) => {
+        procedureArray?.push(doc.data());
+      });
+      setProcedureData(procedureArray);
+    }
+    allProcedures();
+  }, [currentUser?.uid]);
+  console.log("PROCEDUREDATA", procedureData);
+
+  return (
+    <>
+      <BasicTabs />
+    </>
+  );
+}
