90e2a72f2f64c445694bda43eebc7fcbc45a0a42
Perry-E <perryeber@gmail.com>
2021-11-26 03:20:18 +0800

    Log In removed console logs and commented code

--- dentaltracker/src/Components/LogIn/Index.jsx
+++ dentaltracker/src/Components/LogIn/Index.jsx
@@ -11,7 +11,6 @@ export default function LogIn() {
   const [error, setError] = useState("");
 
   const currentUser = useAuth();
-  console.log("LOGIN 1 CURRENT USER", currentUser);
 
   let navigate = useNavigate();
 
@@ -19,8 +18,7 @@ export default function LogIn() {
     setLoading(true);
     try {
       await login(emailRef.current.value, passwordRef.current.value);
-      console.log("CURRENT USER LOGIN", currentUser?.email);
-      navigate("/dashboard")
+      navigate("/dashboard");
     } catch {
       setError("Failed To Log In, Try Again!");
     }
@@ -28,12 +26,10 @@ export default function LogIn() {
   }
 
   return (
-    <div className="w-100" style={{ maxWidth: "400px", marginTop: "-200px", }} >
+    <div className="w-100" style={{ maxWidth: "400px", marginTop: "-200px" }}>
       <Card>
         <Card.Body>
-          {/* center text, margin bottom */}
           <h2 className="text-center mb-4">Log In</h2>
-          {/* <div>Currently logged in as: {currentUser?.email}</div> */}
           {error && <Alert variant="danger">{error}</Alert>}
 
           <Form>
@@ -45,21 +41,19 @@ export default function LogIn() {
               <Form.Label>Password:</Form.Label>
               <Form.Control type="password" ref={passwordRef} required />
             </Form.Group>
-            {/* <Link to="/dashboard"> */}
-              <Button
-                disabled={loading || currentUser}
-                onClick={handleLogin}
-                className="w-100 mt-4"
-                type="submit"
-                style={{
-                  backgroundColor: "#06d6a0",
-                  color: "#495057",
-                  border: "none",
-                }}
-              >
-                Log In
-              </Button>
-            {/* </Link> */}
+            <Button
+              disabled={loading || currentUser}
+              onClick={handleLogin}
+              className="w-100 mt-4"
+              type="submit"
+              style={{
+                backgroundColor: "#06d6a0",
+                color: "#495057",
+                border: "none",
+              }}
+            >
+              Log In
+            </Button>
           </Form>
           <div className="w-100 text-center mt-2">
             <Link to="/forgot-password">Forgot Password?</Link>
@@ -72,47 +66,3 @@ export default function LogIn() {
     </div>
   );
 }
-
-//   async function handleSignup(e) {
-//     e.preventDefault();
-//     setLoading(true);
-//     // try {
-//     //   if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-//     //     return setError("Passwords Do Not Match");
-//     //   }
-//       await login(emailRef.current.value, passwordRef.current.value);
-//     // } catch {
-//     //   setError("Failed To Create An Account");
-//     // }
-//     setLoading(false);
-//   }
-
-// async function handleLogout() {
-//     setLoading(true);
-//     try {
-//         await logout();
-//     } catch {
-//         alert("Error!");
-//     }
-//     setLoading(false);
-// }
-
-//   <div id="fields">
-//     <input ref={emailRef} placeholder="Email" />
-//     <input ref={passwordRef} type="password" placeholder="Password" />
-//     <input
-//     ref={passwordConfirmRef}
-//     type="password"
-//     placeholder="Password Confirmation"
-//     />
-//     </div>
-
-//     <Button disabled={loading || currentUser} onClick={handleSignup}>
-//     Sign Up
-//     </Button>
-//     <Button disabled={loading || currentUser} onClick={handleLogin}>
-//     Log In
-//     </Button>
-//     <Button disabled={loading || !currentUser} onClick={handleLogout}>
-//     Log Out
-// </Button>
