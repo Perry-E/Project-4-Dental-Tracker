f6cae68b7803b5f6dac286fd477c117db17ec53e
Perry-E <perryeber@gmail.com>
2021-11-25 23:41:30 +0800

    Adjusted sign up messages and navigation

--- dentaltracker/src/Components/SignUp/Index.jsx
+++ dentaltracker/src/Components/SignUp/Index.jsx
@@ -1,10 +1,10 @@
 import { useRef, useState } from "react";
 import { Card, Form, Button, Alert } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { signup, useAuth } from "../../firebase";
 
 import { addDoc, collection } from "firebase/firestore";
-import {db} from "../../firebase";
+import { db } from "../../firebase";
 
 export default function SignUp() {
   const emailRef = useRef();
@@ -13,28 +13,34 @@ export default function SignUp() {
 
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [confirmPassword, setConfirmPassword] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const currentUser = useAuth();
+  let navigate = useNavigate();
 
   async function handleSignup(e) {
     e?.preventDefault();
-    setLoading(true);
-    // try {
+
+    // setLoading(true);
     if (passwordRef.current.value !== passwordConfirmRef.current.value) {
       return setError("Passwords Do Not Match");
     }
-    await signup(emailRef.current.value, passwordRef.current.value);
-    console.log("REFS", email, password, confirmPassword)
-    console.log("CURRENT USER SIGN UP", currentUser?.email);
-    // } catch {
-    //   setError("Failed To Create An Account");
-    // }
+
+    try {
+      setError("");
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+      navigate("/location");
+      console.log("REFS", email, password, confirmPassword);
+      console.log("CURRENT USER SIGN UP", currentUser?.email);
+    } catch {
+      setError("Failed To Create An Account");
+    }
     setLoading(false);
   }
-  
+
   // async function handleNew(e) {
   //   e?.preventDefault();
   //     await addDoc(collection(db, "users"), {
@@ -47,40 +53,60 @@ export default function SignUp() {
   // }
 
   return (
-    <div className="w-100" style={{ maxWidth: "400px" }}>
+    <div className="w-100" style={{ maxWidth: "400px", marginTop: "-400px" }}>
       <Card>
         <Card.Body>
           {/* center text, margin bottom */}
           <h2 className="text-center mb-4">Sign Up</h2>
-          <div>Currently logged in as: {currentUser?.email}</div>
+          {/* <div>Currently logged in as: {currentUser?.email}</div> */}
           {error && <Alert variant="danger">{error}</Alert>}
 
           <Form>
             <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required onChange={(e)=>setEmail(e.target.value)}/>
+              <Form.Label>Email:</Form.Label>
+              <Form.Control
+                type="email"
+                ref={emailRef}
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </Form.Group>
             <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required onChange={(e)=>setPassword(e.target.value)}/>
+              <Form.Label>Password:</Form.Label>
+              <Form.Control
+                type="password"
+                ref={passwordRef}
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </Form.Group>
             <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required onChange={(e)=>setConfirmPassword(e.target.value)}/>
+              <Form.Label>Password Confirmation:</Form.Label>
+              <Form.Control
+                type="password"
+                ref={passwordConfirmRef}
+                required
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
             </Form.Group>
-            <Link to="/location">
-              <Button
-                disabled={loading || currentUser}
-                onClick={() => {
-                  handleSignup();
-                  // handleNew();
-                }}
-                className="w-100 mt-4"
-                type="submit"
-              >
-                Sign Up
-              </Button>
-            </Link>
+            {/* <Link to="/location"> */}
+            <Button
+              disabled={loading || currentUser}
+              onClick={() => {
+                handleSignup();
+                // handleNew();
+              }}
+              className="w-100 mt-4"
+              type="submit"
+              style={{
+                backgroundColor: "#06d6a0",
+                color: "#495057",
+                border: "none",
+              }}
+            >
+              Sign Up
+            </Button>
+            {/* </Link> */}
           </Form>
         </Card.Body>
       </Card>
